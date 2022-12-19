9d66de577bee1814b55b0a81103f3fd47c603f93
songiwon <biob1121@naver.com>
2022-12-19 15:40:01 +0900

    chore: 프로젝트 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <></>
+      <Routes></Routes>
     </div>
   );
 }
