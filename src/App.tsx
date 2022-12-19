a222479b6c1e553c4fc85665b884302bc738cc30
songiwon <biob1121@naver.com>
2022-12-20 06:12:52 +0900

    feat: 글로벌 스타일 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Main from "./pages/Main";
+import GlobalStyle from "./styles/GlobalStyle";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      <GlobalStyle />
       <Routes>
         <Route path="/main" element={<Main />} />
       </Routes>
---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,15 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  * {
+    font-family: 'Pretendard';
+    font-style: normal;
+  }
+
+  body {
+    margin:0px;
+    padding:0px;
+  }
+`;
+
+export default GlobalStyle;
