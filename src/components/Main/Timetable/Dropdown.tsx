b06fc63067ed6ece9213574874a8f9f760db8dc1
songiwon <biob1121@naver.com>
2022-12-20 13:53:22 +0900

    feat: Dropdown 퍼블리싱 완료

--- src/components/Main/Timetable/Dropdown.tsx
+++ src/components/Main/Timetable/Dropdown.tsx
@@ -7,11 +7,15 @@ const Dropdown = () => {
     <_Dropdown>
       <span>1학년</span>
       <Arrow direction={false} />
+      <_MenuBox>
+        <div>1학년</div>
+      </_MenuBox>
     </_Dropdown>
   );
 };
 
 const _Dropdown = styled.div`
+  position: relative;
   cursor: pointer;
   display: flex;
   align-items: center;
@@ -27,6 +31,41 @@ const _Dropdown = styled.div`
     font-weight: 500;
     font-size: 16px;
     color: #242424;
+  }
+`;
+
+const _MenuBox = styled.div`
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: absolute;
+  top: 47px;
+  left: 0px;
+  right: 0px;
+  width: 160px;
+  background: #ffffff;
+  border: 1px solid #9e9e9e;
+  border-radius: 5px;
+
+  div {
+    transition: all 0.3s;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    font-weight: 500;
+    font-size: 16px;
+    color: #242424;
+    padding-left: 15px;
+    display: flex;
+    width: 145px;
+    height: 40px;
+    border-bottom: 1px solid #e8e8e8;
+    background-color: #ffffff;
+
+    &:hover {
+      background-color: #f5f5f5;
+    }
   }
 `;
 
