a8dbdd51e0b91486941c6927b36878db03e3958c
songiwon <biob1121@naver.com>
2022-12-27 22:44:30 +0900

    feat: 개발

--- src/apis/getChangeRecords.ts
+++ src/apis/getChangeRecords.ts
@@ -2,7 +2,7 @@ import { instance } from "./axios";
 import { paramsType } from "../interfaces/records";
 
 const getChangeRecords = (params: paramsType) => {
-  const response = instance.get("/docs/template", { params: params });
+  const response = instance.get("/records/list", { params: params });
   return response;
 };
 
