c270bff5064401bfd9e4bde2eb7b346e2cbdd724
songiwon <biob1121@naver.com>
2022-12-27 19:32:06 +0900

    fix: 병합 오류 변경

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,8 @@ function App() {
       <Routes>
         <Route path="/main" element={<Main />} />
         <Route path="/signUp" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} /> 
-        
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/records" element={<Records />} />
       </Routes>
     </div>
   );
